0dc772de88457cf676ec5a1a8ee9afc61a1cfd2a
Alexandr Moroz <afmoroz@gmail.com>
2020-02-07 23:44:40 +0200

    small change in levels move

--- src/handlers/levelsMove.tsx
+++ src/handlers/levelsMove.tsx
@@ -7,25 +7,30 @@ export function moveBtwLevels(e: any, step: number) {
   let currentLevel = parseInt(currentEl.dataset.level);
   const parentLevel = currentLevel === -1 ? -1 : currentLevel - 1;
   const parentParentLevel = parentLevel === -1 ? -1 : parentLevel - 1;
-  const parentEl = currentEl.closest(`[data-level="${parentLevel}"]`)
-  const parentParentEl = parentEl.closest(`[data-level="${parentParentLevel}"]`)
+  const parentEl = currentEl.closest(`[data-level="${parentLevel}"]`);
+  const parentParentEl = parentEl.closest(
+    `[data-level="${parentParentLevel}"]`
+  );
   const parentIndex = parentEl.tabIndex;
   const next = currentLevel + step;
   let tabbables = [];
   if (step > 0) {
     tabbables = currentEl.querySelectorAll(`[data-level="${next}"]`);
+    if (tabbables.length > 0) {
+      const nextElement = tabbables[0];
+      nextElement.focus();
+    } else {
+      // do nothing
+    }
   } else if (step < 0) {
     tabbables = parentParentEl.querySelectorAll(
       `[data-level="${next}"][tabIndex="${parentIndex}"]`
     );
-  }
-
-  const l = tabbables.length;
-  if (l > 0) {
-    const nextElement = tabbables[0];
-    nextElement.focus();
-  } else {
-    // do nothing
-    parentEl.focus();
+    if (tabbables.length > 0) {
+      const nextElement = tabbables[0];
+      nextElement.focus();
+    } else {
+      parentEl.focus();
+    }
   }
 }
